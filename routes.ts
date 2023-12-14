d075bc5269721b260c83a37de1d0a8ee5e1fc444
Alexandre Silva Santos <alex_ff2@hotmail.com>
2023-12-14 13:37:19 -0300

    add new routes

--- routes.ts
+++ routes.ts
@@ -6,9 +6,30 @@ const routes = express.Router()
 routes.get('/', (require, response) => {
   return response.send('Conectado com sucesso!')
 })
-//routes.get('/representative', (require, response) => {})
-//routes.get('/representative/description', representativeController.findByDescription)
-//routes.get('/customers/:idRepresentative', customerController.findByRepresentative)
+routes.get('/representative', async (require, response) => {
+  const { data } = await api.get('/representative')
+  return response.json(data)
+})
+routes.get('/representative/description', async (require, response) => {
+  const { description } = require.query
+
+  const { data } = await api.get('/representative/description', {
+    params: {
+      description
+    }
+  })
+
+  return response.json(data)
+})
+routes.get('/customers/:idRepresentative', async (require, response) => {
+  const { idRepresentative } = require.params
+  const { idShop, route } = require.query
+
+  const { data } = await api.get(`/customers/${idRepresentative}`, {
+    params: { idShop, route }
+  })
+  return response.json(data)
+})
 routes.get('/report/dre', async (require, response) => {
   try {
     const { dataInicial, dataFinal } = require.query
@@ -24,6 +45,14 @@ routes.get('/report/dre', async (require, response) => {
     return response.status(400).json(error)
   }
 })
-//routes.get('/report/customerByRepresentative', customersByRepresentativeController.index)
+routes.get('/report/customerByRepresentative', async (require, response) => {
+  const { date, idRepresentative, idEstablishment, idShop, route } = require.query
+
+  const { data } = await api.get('/report/customerByRepresentative', {
+    params: { date, idRepresentative, idEstablishment, idShop, route }
+  })
+
+  return response.json(data)
+})
 
 export default routes
